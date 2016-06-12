b534ad23ccb0de83c1286d8a6ce863f01157d585
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2016-06-12 22:21:23 +0900

    add lud.cpp
    LU分解のプログラムを新たに作成した．ピボット調整はなし．

---
+++ lud.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+
+short matPrint(double* mat, short M, short N);
+
+int main(){
+
+  double A[25]={
+    1.0, 2.0, 3.0, 4.0, 5.0,
+    4.0, 5.0, 6.0, 7.0, 8.0,
+    6.0, 8.0, 9.0, 10.0, 11.0,
+    12.0, 13.0, 14.0, 15.0, 16.0,
+    17.0, 18.0, 19.0, 20.0, 21.0
+  };
+  double B[25]={0.0};
+
+  double sum;
+  short i,j,k;
+  short m=5;
+  short n=5;
+
+  for (i=0; i<m; i++)
+  {
+    for (j=0; j<n; j++)
+    {
+      sum=0.0;
+      for (k=0; k<i; k++)
+      {
+        sum=sum+B[i*m+k]*B[k*m+j];
+      }
+
+      if(i<=j)
+      {
+        B[i*m+j]=A[i*m+j]-sum;
+      }
+      else
+      {
+        B[i*m+j]=(A[i*m+j]-sum)/B[j*m+j];
+      }
+    }
+  }
+
+  matPrint(A, m, n);
+  matPrint(B, m, n);
+
+}
+
+short matPrint(double* mat, short M, short N){
+  short i,j;
+  for (i=0;i<M;i++){
+    for (j=0;j<N;j++){
+      printf("%8.5f " ,mat[i*N+j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
